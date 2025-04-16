4f9a81d34551b2ff9b7f60cd85deba9ebffef200
lilwanan <jba0018@alu.medac.es>
2025-04-16 13:01:07 +0200

    Añado patrón de contraseñas

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, redirect, request, jsonify,  render_template, url_for
 from flask import session
 import requests
--- static/styles/register.js
+++ static/styles/register.js
@@ -8,14 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
   const nextToFinal = document.getElementById('next-to-final');
   const prevToLoginFinal = document.getElementById('prev-to-login-final');
 
+  
+
   nextToLogin.addEventListener('click', function () {
     // Tenemos que validar que ha relleneado el formulario de registro de email y contraseña , además de que los campos de contraseña y confirmar contraseña son iguales
     if( !document.getElementById('email').value.trim() || !document.getElementById('contrasena').value.trim()  || !document.getElementById('confirm').value.trim() ) {
       alert('Por favor, rellena todos los campos');
       return;
     }else{
+      var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%\^&\*])(?=.{8,})");
+      
       const password = document.getElementById('contrasena').value.trim();
       const confirmPassword = document.getElementById('confirm').value.trim();
+      if(!strongRegex.test(password)) {
+        alert("La contraseña no sigue el patron establecido")
+        return;
+      }
       if (password !== confirmPassword) {
         alert('Las contraseñas no coinciden');
         return;
