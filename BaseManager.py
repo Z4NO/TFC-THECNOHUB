94fcc9efbbe5a702f3c54a2665882ba8c5c7c583
lilwanan <jba0018@alu.medac.es>
2025-04-17 15:17:42 +0200

    compruebo que el correo exista

--- BaseManager.py
+++ BaseManager.py
@@ -6,11 +6,13 @@ from User import User
 from Encripter import Encripter
 import os
 
+
 class BaseManager:
     def __init__(self):
         try:
-            self.cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), "tfgforo-firebase-adminsdk-fbsvc-61fd334c13.json"))
-            self.encripter =  Encripter(os.getenv('MASTER_KEY').encode())
+            self.cred = credentials.Certificate(os.path.join(os.path.dirname(
+                __file__), "tfgforo-firebase-adminsdk-fbsvc-61fd334c13.json"))
+            self.encripter = Encripter(os.getenv('MASTER_KEY').encode())
             if not firebase_admin._apps:
                 firebase_admin.initialize_app(self.cred)
             self.db = firestore.client()
@@ -21,6 +23,9 @@ class BaseManager:
 
     def _add_user(self, user: User):
         try:
+            if self._email_exists(user.email):
+                print("El email ya esta registrado")
+                return False
             # Crear un nuevo documento en la colección 'users'
             self.db.collection('usuario').add({
                 'contrasena': self.encripter._encript(user.contrasena),
@@ -35,19 +40,31 @@ class BaseManager:
             print(f"Error al añadir el usuario: {e}")
             return False
 
-    def _check_credentials(self,email, password) -> bool:
+    def _check_credentials(self, email, password) -> bool:
         try:
             user_ref = self.db.collection('usuario')
             user_ref_query = user_ref.where('email', '==', email).stream()
             # traemos la contrasena encriptada de la base de datos y la desencriptamos para compararla
             for doc in user_ref_query:
                 user_data = doc.to_dict()
-                contraseña_desencriptada = self.encripter._decript(user_data['contrasena'])
-                break  
+                contraseña_desencriptada = self.encripter._decript(
+                    user_data['contrasena'])
+                break
             if contraseña_desencriptada == password:
-                return True 
+                return True
             else:
                 return False
         except Exception as e:
             print(f"Error al verificar las credenciales: {e}")
+            return False
+
+    def _email_exists(self, email: str) -> bool:
+        try:
+            user_ref = self.db.collection('usuario')
+            user_ref_query = user_ref.where('email', '==', email).stream()
+            for doc in user_ref_query:
+                return True
+            return False
+        except Exception as e:
+            print(f"Error al comprobar si el email existe: {e}")
             return False
