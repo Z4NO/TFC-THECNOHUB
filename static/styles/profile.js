306fa39e31a3eecf77ec9f852771d3a143e18d7f
Z4NO <danixt35@gmail.com>
2025-04-23 12:19:25 +0200

    Aparencia del perfil

--- static/styles/profile.js
+++ static/styles/profile.js
@@ -1,5 +1,36 @@
+function cargarComponente(action) {
+  const contenedorOpcion = document.querySelector('.contenedor-opcion');
+  let url = '';
+  switch (action) {
+    case 'perfil':
+      url = '/static/components/opciones-perfil.html';
+      break;
+    case 'actividad':
+      url = '/static/components/opciones-actividad.html';
+      break;
+    default:
+      contenedorOpcion.innerHTML = `<p>Selecciona una opción para ver su contenido.</p>`;
+      return;
+  }
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('No se pudo cargar el componente');
+      }
+      return response.text();
+    })
+    .then(html => {
+      contenedorOpcion.innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Error al cargar el componente:', error);
+      contenedorOpcion.innerHTML = `<p>Error al cargar el contenido.</p>`;
+    });
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
-    const contenedorOpcion = document.querySelector('.contenedor-opcion');
     const buttons = document.querySelectorAll('.button-container .button');
   
   
@@ -10,35 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   
-    function cargarComponente(action) {
-      let url = '';
-      switch (action) {
-        case 'perfil':
-          url = '/static/components/opciones-perfil.html';
-          break;
-        case 'actividad':
-          url = '/static/components/opciones-actividad.html';
-          break;
-        default:
-          contenedorOpcion.innerHTML = `<p>Selecciona una opción para ver su contenido.</p>`;
-          return;
-      }
-  
-      fetch(url)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('No se pudo cargar el componente');
-          }
-          return response.text();
-        })
-        .then(html => {
-          contenedorOpcion.innerHTML = html;
-        })
-        .catch(error => {
-          console.error('Error al cargar el componente:', error);
-          contenedorOpcion.innerHTML = `<p>Error al cargar el contenido.</p>`;
-        });
-    }
 });
 
 window.addEventListener('load', function () {
